7d3e7f88cfe61a42cfc57d5cb71c6459090bd8ff
Andre Le <andrelezx@gmail.com>
2021-07-29 10:45:27 -0600

    Add resize button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function makeGrid() {
-    const grid = document.querySelector("#Grid-Container");
-    for (let i = 0; i < 256; ++i) {
+function makeGrid(sideLength = 16) {
+    const grid = document.getElementById("Grid-Container");
+    const gridArea = Math.pow(sideLength, 2);
+    for (let i = 0; i < gridArea; ++i) {
         const div = document.createElement("div");
         div.setAttribute("id", i);
         div.classList.add("Grid-Box");
@@ -9,6 +10,17 @@ function makeGrid() {
         );
         grid.appendChild(div);
     }
+    grid.style.gridTemplateColumns = `repeat(${sideLength}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${sideLength}, 1fr)`;
+}
+
+function resizeGrid() {
+    const sideLength = prompt("Number of squares per side: ");
+    if (sideLength > 100) {
+        alert("That number is too high");
+        return;
+    }
+    makeGrid(sideLength);
 }
 
 makeGrid();
