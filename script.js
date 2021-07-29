48d9206b792e812e3cad3e84a264fbd06f29165d
Andre Le <andrelezx@gmail.com>
2021-07-29 11:36:34 -0600

    Add color modes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function makeGrid(sideLength = 16) {
     const grid = document.querySelector("#Grid-Container");
+    // This is to prevent grids from accumulating
     while (grid.hasChildNodes()) {
         const childNode = grid.lastElementChild;
         grid.removeChild(childNode);
@@ -10,13 +11,17 @@ function makeGrid(sideLength = 16) {
         const div = document.createElement("div");
         div.setAttribute("id", i);
         div.classList.add("Grid-Box");
-        div.addEventListener("mouseover", () => 
-            div.classList.add("hover")
-        );
+        div.addEventListener("mouseover", turnWhite);
         grid.appendChild(div);
     }
     grid.style.gridTemplateColumns = `repeat(${sideLength}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${sideLength}, 1fr)`;
+}
+
+function turnWhite() {this.style.backgroundColor = "white";}
+function turnRainbow() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    this.style.backgroundColor = "#" + randomColor;
 }
 
 function resizeGrid() {
@@ -26,6 +31,22 @@ function resizeGrid() {
         return;
     }
     makeGrid(sideLength);
+}
+
+function changeWhite() {
+    const boxes = document.querySelectorAll(".Grid-Box");
+    boxes.forEach(function (box) {
+        box.removeEventListener("mouseover", turnRainbow);
+        box.addEventListener("mouseover", turnWhite);
+    });
+}
+
+function changeRainbow() {
+    const boxes = document.querySelectorAll(".Grid-Box");
+    boxes.forEach(function (box) {
+        box.removeEventListener("mouseover", turnWhite);
+        box.addEventListener("mouseover", turnRainbow);
+    });
 }
 
 makeGrid();
